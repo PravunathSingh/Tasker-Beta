2cb4c8756258c6adf1eb4c2cfa77b4939fd9fc3f
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-26 02:07:19 +0530

    Added the About.js

---
+++ src/components/layout/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h4>Beta Version 1.0.0</h4>
+            <a href="/" className="btn btn-info">Back To Home</a>
+        </div>
+    )
+}
+
+export default About
