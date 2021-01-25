a708d6c68522f87ead9fa51ae908ed94c4bc5da0
Pravunath Singh <pravunathsingh@gmail.com>
2021-01-25 23:52:49 +0530

    Added the Header.js

---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <button className="btn btn-success btn-block btn-lg float-right mb-4">Add Task</button>
+        </div>
+    )
+}
+
+export default Header
